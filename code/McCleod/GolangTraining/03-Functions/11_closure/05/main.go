a84bc0a3a10fae0f6bdcc129436f96c5a0055fc2
Clay Dunston <dunstontc@gmail.com>
2018-08-10 11:09:33 -0500

    formatting

--- code/McCleod/GolangTraining/03-Functions/11_closure/05/main.go
+++ code/McCleod/GolangTraining/03-Functions/11_closure/05/main.go
@@ -21,7 +21,7 @@ func main() {
 }
 
 /*
-closure helps us limit the scope of variables used by multiple functions
-without closure, for two or more funcs to have access to the same variable,
-that variable would need to be package scope
-*/
+ * closure helps us limit the scope of variables used by multiple functions
+ * without closure, for two or more funcs to have access to the same variable,
+ * that variable would need to be package scope
+ */
